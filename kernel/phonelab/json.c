0906de6b1c728d294338110d94fa493b848cfdb3
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2015-10-17 10:48:27 -0400

    Moved all memory operations to within json.c

--- kernel/phonelab/json.c
+++ kernel/phonelab/json.c
@@ -13,11 +13,49 @@
 #include "json.h"
 #endif
 
-#include "mem.h"
-
 #ifdef __KERNEL__
 #define printf(...) printk(KERN_INFO __VA_ARGS__);
 #endif
+
+
+static inline void *alloc(size_t size)
+{
+	void *buffer = NULL;
+#ifdef __KERNEL__
+	buffer = kmalloc(size, GFP_KERNEL);
+#else
+	buffer = malloc(size);
+#endif
+	return buffer;
+}
+
+static inline void release(void *ptr)
+{
+#ifdef __KERNEL__
+	kfree(ptr);
+#else
+	free(ptr);
+#endif
+}
+
+static inline char *stringdup(const char *str)
+{
+#ifdef __KERNEL__
+	return kstrdup(str, GFP_KERNEL);
+#else
+	return strdup(str);
+#endif
+}
+
+static inline void *grealloc(void *ptr, size_t size)
+{
+#ifdef __KERNEL__
+	return krealloc(ptr, size, GFP_KERNEL);
+#else
+	return realloc(ptr, size);
+#endif
+}
+
 
 struct json_object *json_create_object(void)
 {
