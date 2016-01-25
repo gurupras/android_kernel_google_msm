f2ab9d473568ab5bb7abe41ac600058816c8f8cd
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2016-01-25 13:35:39 -0500

    Updated periodic_ctx_switch_info event logic

--- include/trace/events/phonelab.h
+++ include/trace/events/phonelab.h
@@ -54,26 +54,45 @@ TRACE_EVENT(phonelab_periodic_ctx_switch_info,
 	TP_ARGS(task, cpu),
 
 	TP_STRUCT__entry(
-		__field( struct task_struct *,	task	)
-		__field( unsigned long,		utime	)
-		__field( unsigned long,		stime	)
-		__field( int,	cpu			)
+		__field( int,	cpu				)
+		__field( pid_t,	pid				)
+		__field( pid_t,	tgid				)
+		__array( char,	comm,	TASK_COMM_LEN		)
+		__field( unsigned long,		utime_t		)
+		__field( unsigned long,		stime_t		)
+		__field( unsigned long,		utime		)
+		__field( unsigned long,		stime		)
+		__field( unsigned long,		cutime_t	)
+		__field( unsigned long,		cstime_t	)
+		__field( unsigned long,		cutime		)
+		__field( unsigned long,		cstime		)
 	),
 
 	TP_fast_assign(
-		__entry->task = task;
-		__entry->cpu  = cpu;
-		task_times(__entry->task, &__entry->utime, &__entry->stime);
+		__entry->cpu	= cpu;
+
+		__entry->pid	= task->pid;
+		__entry->tgid	= task->tgid;
+		memcpy(__entry->comm, task->comm, TASK_COMM_LEN);
+
+		task_times(task, &__entry->utime_t, &__entry->stime_t);
+		__entry->utime	= cputime_to_clock_t(task->utime);
+		__entry->stime	= cputime_to_clock_t(task->stime);
+
+		__entry->cutime_t	= task->signal->cutime;
+		__entry->cstime_t	= task->signal->cstime;
+		__entry->cutime	= cputime_to_clock_t(task->signal->cutime);
+		__entry->cstime	= cputime_to_clock_t(task->signal->cstime);
 	),
 
-	TP_printk("cpu=%d pid=%d tgid=%d comm=%s utime_t=%lu stime_t=%lu cutime=%lu cstime=%lu"
+	TP_printk("cpu=%d pid=%d tgid=%d comm=%s utime_t=%lu stime_t=%lu utime=%lu stime=%lu"
 		"cutime_t=%lu cstime_t=%lu cutime=%lu cstime=%lu",
 		__entry->cpu,
-		__entry->task->pid, __entry->task->tgid, __entry->task->comm,
-		__entry->task->utime, __entry->task->stime,
-		cputime_to_clock_t(__entry->utime), cputime_to_clock_t(__entry->stime),
-		__entry->task->signal->cutime, __entry->task->signal->cstime,
-		cputime_to_clock_t(__entry->task->signal->cutime), cputime_to_clock_t(__entry->task->signal->cstime))
+		__entry->pid, __entry->tgid, __entry->comm,
+		__entry->utime_t, __entry->stime_t,
+		__entry->utime, __entry->stime,
+		__entry->cutime_t, __entry->cstime_t,
+		__entry->cutime, __entry->cstime)
 );
 
 TRACE_EVENT(phonelab_proc_foreground,
